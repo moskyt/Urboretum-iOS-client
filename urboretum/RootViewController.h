d01fd08caf5ad48955f5d57b25c77530dd1fd309
Frantisek Havluj <moskyt@rozhled.cz>
2011-10-29 00:38:08 +0200

    Initial Commit

---
+++ urboretum/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  urboretum
+//
+//  Created by František Havlůj on 29.10.//11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController
+
+@end
---
+++ urboretum/urboretumAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  urboretumAppDelegate.h
+//  urboretum
+//
+//  Created by František Havlůj on 29.10.//11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface urboretumAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
